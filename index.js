e52ad0e7fd096c1fa8e8e4f8a802434f07e49e70
stutee <email@address.com>
2022-06-30 02:13:00 +0545

    add port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const bodyParser = require("body-parser");
